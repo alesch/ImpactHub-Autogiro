1fca9817af2d6d7f0f8f442dc53819ce7bef476e
Alex Schenkman <alex@schenkman.info>
2015-05-27 22:49:05 +0200

    Added testing with wallaby.

---
+++ tests/seal.js
@@ -0,0 +1,13 @@
+var assert = chai.assert, _ = Underscore();
+
+describe('one', function () {
+
+    it('should say yes', function () {
+        var x = 42;
+        if (_.isUndefined(x)) {
+        }
+
+        assert.equal(a(), 42, 'caramelo');
+    });
+});
+
---
+++ wallaby.js
@@ -0,0 +1,25 @@
+/*
+ module.exports = function () {
+ return {
+ files: [
+ 'server/seal.js',
+ 'tests/lib/chai.js',
+ 'tests/lib/underscore-min.js'
+ ],
+
+ tests: [
+ 'tests/seal.js'
+ ],
+
+ testFramework: 'mocha@2.0.1',
+
+ */
+/*
+ bootstrap: function (wallaby) {
+ var mocha = wallaby.testFramework;
+ mocha.ui('tdd');
+ }
+ *//*
+
+ };
+ };*/
