8390fb6aa42af2e798b5df38b9d042fec0897882
Alex Schenkman <alex@schenkman.info>
2015-05-27 22:02:36 +0200

    Fixed login.

--- client/Login.js
+++ client/Login.js
@@ -14,3 +14,14 @@ Template.Login.onRendered(function () {
             on      : 'change'
         });
 });
+
+Template.Login.events({
+    'click #LoginButton' : function (event, template) {
+        var credentials = {
+            username: $('input[name="username"]').val(),
+            password: $('input[name="password"]').val()
+        };
+        Session.set('user', credentials);
+        return false;
+    }
+});
